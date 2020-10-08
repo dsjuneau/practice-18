d73055a7b5bd38a42e29a80433a61742cf28f4c4
Scott Juneau <dsjuneau@gmail.com>
2020-10-08 06:31:49 -0500

    basics done

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# practice-18
+# Canvas
+
+Do the following:
+
+1.  Set up a page with canvas taking up the full screen with no scroll bars.
+2.  Add a background color and one circle.
+3.  Add another circle upon click event.
---
+++ app.js
@@ -0,0 +1,24 @@
+const canvas = document.querySelector("canvas");
+console.log(canvas);
+let c = canvas.getContext("2d");
+
+const sizeWindow = () => {
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+};
+
+sizeWindow();
+window.onresize = sizeWindow;
+
+const drawCircle = (x, y, r) => {
+  c.fillStyle = "red";
+  c.beginPath();
+  c.ellipse(x, y, r, r, 0, 0, 2 * Math.PI);
+  c.fill();
+};
+
+drawCircle(100, 200, 50);
+
+canvas.addEventListener("click", (e) => {
+  drawCircle(e.clientX, e.clientY, 50);
+});
