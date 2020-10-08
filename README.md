02c662cc26c454d2343f6ee256f42618f15c2b69
Scott Juneau <dsjuneau@gmail.com>
2020-10-08 16:16:35 -0500

    added drawing feature

--- README.md
+++ README.md
@@ -5,3 +5,4 @@ Do the following:
 1.  Set up a page with canvas taking up the full screen with no scroll bars.
 2.  Add a background color and one circle.
 3.  Add another circle upon click event.
+4.  Set up a new page that allows a user to draw with the mouse.
---
+++ app2.js
@@ -0,0 +1,36 @@
+const canvas = document.querySelector("canvas");
+console.log(canvas);
+let c = canvas.getContext("2d");
+
+const sizeWindow = () => {
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+};
+
+sizeWindow();
+window.onresize = sizeWindow;
+
+let drawing = false;
+c.strokeStyle = "white";
+c.fillStyle = "white";
+const bLine = (e) => {
+  drawing = true;
+  c.beginPath();
+  c.moveTo(e.clientX, e.clientY);
+  c.fillRect(e.clientX, e.clientY, 3, 3);
+};
+
+const dLine = (e) => {
+  if (!drawing) return;
+  c.lineWidth = 3;
+  c.lineCap = "round";
+  c.lineTo(e.clientX, e.clientY);
+  c.stroke();
+};
+const eLine = (e) => {
+  drawing = false;
+};
+
+canvas.addEventListener("mousedown", bLine);
+canvas.addEventListener("mouseup", eLine);
+canvas.addEventListener("mousemove", dLine);
