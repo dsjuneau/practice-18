37f06628819256e0846c2277e370ef94ebf0fb4d
Scott Juneau <dsjuneau@gmail.com>
2020-10-10 07:27:33 -0500

    animation working fine

--- app3.js
+++ app3.js
@@ -19,7 +19,43 @@ const drawCircle = (x, y, r) => {
 drawCircle(100, 200, 50);
 
 canvas.addEventListener("click", (e) => {
+    spots.push({
+        x: e.clientX,
+        y: e.clientY,
+        r: 50,
+        dx: Math.floor(Math.random()*7)-3,
+        dy: Math.floor(Math.random()*7)-3
+    })
   drawCircle(e.clientX, e.clientY, 50);
 });
 
+const updateSpot = (s)=>{
+    s.x +=s.dx;
+    s.y +=s.dy;
+    if (s.x+s.r>= canvas.width || s.x-s.r<= 0){
+        s.dx = -s.dx;
+    }
+    if (s.y+s.r>= canvas.height || s.y-s.r<= 0){
+        s.dy = -s.dy;
+    }
+    return s;
+}
 
+const spots = [{
+    x: 100, 
+    y: 200,
+    r: 50,
+    dx: 3, 
+    dy: 4
+}];
+
+const drawIt = ()=>  {
+    c.clearRect(0, 0, canvas.width, canvas.height);
+    spots.forEach(spot =>{
+        drawCircle(spot.x, spot.y, spot.r);
+       spot = updateSpot(spot);
+    })
+    requestAnimationFrame(drawIt);
+}
+
+drawIt();
